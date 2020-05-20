a4187c28356d58a8c6681f224aed3fff18152e78
Sadhana Kalyana Raman <Sadhu4590@gmail.com>
2020-05-20 19:49:10 +1000

    Initial Commit - infra proj

---
+++ MicroRabbit/MicroRabbit.Infra.IoC/DependencyContainer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Extensions.DependencyInjection;
+using System.Text;
+using MicroRabbit.Domail.Core.Bus;
+using MicroRabbit.Infra.Bus;
+
+namespace MicroRabbit.Infra.IoC
+{
+    class DependencyContainer
+    {
+        public static void RegisterServices(IServiceCollection services)
+        {
+            //Domain Bus
+            services.AddTransient<IEventBus, RabbitMQBus>();
+        }
+    }
+}
